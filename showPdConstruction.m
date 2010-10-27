ada56bc914026f248939a21ce2ccfa3e14658ca9
George Fraser <gwf2@pitt.edu>
2010-10-26 20:38:14 -0400

    Switching showPdConstruction to polar again.

--- showPdConstruction.m
+++ showPdConstruction.m
@@ -1,10 +1,10 @@
 function showPdConstruction(coeff, controlRateMean, perturbRateMean, controlPd)
 delete('PD Construction.ps');
-for day=1:length(coeff)
+for day=1%:length(coeff)
     % Synchronize field name and order
     cFields = fieldnames(controlRateMean{day});
     pFields = fieldnames(perturbRateMean{day});
-    commonFields = sort(intersect(cFields,pFields));
+    commonFields = sort(intersect(fieldnames(coeff{day}),intersect(cFields,pFields)));
     controlRateMean{day} = orderfields(rmfield(controlRateMean{day},setdiff(cFields,commonFields)));
     perturbRateMean{day} = orderfields(rmfield(perturbRateMean{day},setdiff(pFields,commonFields)));
     % Create data matrices
@@ -22,8 +22,8 @@ for day=1:length(coeff)
     
     % For each unit that is used in control, do a bunch of plots
     controlFields = fieldnames(controlPd{day});
-    controlFields = controlFields(1:5);
-    nPlots = sum(ismember(commonFields,controlFields));
+    controlFields = controlFields(1:min(length(controlFields),10));
+    nPlots = 20;%sum(ismember(commonFields,controlFields))*2;
     plotRow = 0;
     clf;
     for unit=find(ismember(commonFields,controlFields))'
@@ -34,12 +34,12 @@ for day=1:length(coeff)
         shift = find(r==max(r));
         shift = mod((shift-8:shift+8)-1,16)+1;
 
-        subplot(nPlots,2,(plotRow-1)*2+1);
+        subplot(nPlots/4,4,(plotRow-1)*2+1);
         plotGroup(R_c, L_c, C, meanR_c, unit, shift, ymax)
-        subplot(nPlots,2,(plotRow-1)*2+2);
+        subplot(nPlots/4,4,(plotRow-1)*2+2);
         plotGroup(R_p, L_p, C, meanR_p, unit, shift, ymax)
     end
-    set(gcf,'PaperPosition',[0 0 6.9 nPlots*6.9/4]);
+    set(gcf,'PaperPosition',[0 0 6.9 nPlots/2*6.9/8]);
     print -append -dpsc 'PD Construction.ps'
 end
 end
@@ -64,13 +64,16 @@ function plotProfile(tuningImage, ymax, varargin)
 % Only works for 16-target 2d
 th = (-pi+( (0:16)*2*pi/16))';
 r = mean(tuningImage(:,6:15),2);
-plot(th,r,varargin{:});
+% plot(th,r,varargin{:});
 line(th([9 9]),[0 max(r)]);
-xlim([-pi pi]);
-ylim([0 ymax*1.1]);
+% xlim([-pi pi]);
+% ylim([0 ymax*1.1]);
+xlim([-1 1]*ymax*1.1);
+ylim([-1 1]*ymax*1.1);
+axis image;
 xlabel('Target Angle');
 ylabel('Firing Rate');
-% [x,y] = pol2cart(th,r);
-% plot(x([1:end 1]), y([1:end 1]), varargin{:});
+[x,y] = pol2cart(th,r);
+plot(x([1:end 1]), y([1:end 1]), varargin{:});
 end
             
