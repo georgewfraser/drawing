f157ef7736ef09dfdb7eac404862244ff6b43a74
George Fraser <gwf2@pitt.edu>
2010-12-23 12:36:37 -0500

    Velocity directionalTuning.m is not distinguishing between illusion declien and all-condition decline

--- illusionTuning.m
+++ illusionTuning.m
@@ -1,10 +1,9 @@
-function model = illusionTuning(drawingRateMean, drawingSnipsMean, drawingKinMean)
+function [model, channel] = illusionTuning(drawingRateMean, drawingSnipsMean, drawingKinMean)
 model = zeros(0,2);
+channel = [];
 for day=1:length(drawingRateMean)
-    vx = drawingKinMean{day}.velX([1 3 4],:);
-    vy = drawingKinMean{day}.velY([1 3 4],:);
-    motor = [vx(:) vy(:)];
-    motor = bsxfun(@rdivide,motor,sqrt(sum(motor.^2,2)));
+    motor = drawingKinMean{day}.velX([1 3 4],:);
+    motor = motor(:);
     
     illusion = drawingSnipsMean{day}.progress(4,:);
     illusion = (illusion-4)./2;
@@ -14,14 +13,29 @@ for day=1:length(drawingRateMean)
     illusion = illusion.*.8;
     illusion = illusion(:);
     
-    independent = [ones(size(motor,1),1) motor bsxfun(@times,motor,illusion)];
+    independent = [ones(size(motor,1),1) motor motor.*illusion];
     
     cnames = fieldnames(drawingRateMean{day});
+    channel(end+(1:length(cnames))) = cellfun(@(x) str2double(x(5:7)), cnames);
     for unit=1:length(cnames)
         rate = drawingRateMean{day}.(cnames{unit})([1 3 4],:);
         b = regress(rate(:),independent);
-        bnorm = b(2:3)/norm(b(2:3));
         
-        model(end+1,1:2) = [norm(b(2:3)) b(4:5)'*bnorm];
+        model(end+1,1:2) = b(2:3)*sign(b(2));
     end
 end
+gscatter(model(:,1),model(:,2),channel>100,'br','.x',4)
+biggest = sqrt(sum(model.^2,2));
+[biggest,idx] = sort(biggest,'descend');
+for i=1:100
+    text(model(idx(i),1),model(idx(i),2),num2str(idx(i)));
+end
+axis image
+line([0 25],[0 25])
+line([0 25],[0 -25])
+line([0 25],[0 0])
+xlim([0 25]);
+ylim([-25 25])
+xlabel('Motor');
+ylabel('Visual');
+set(gcf,'PaperPosition',[0 0 3.35/2 3.35])
