3e869da7446b96aadfbd3507edbce6b876c494d9
George Fraser <gwf2@pitt.edu>
2011-01-05 14:15:22 -0500

    I think the figures are done

--- showIllusionCovariateQuality.m
+++ showIllusionCovariateQuality.m
@@ -1,4 +1,4 @@
-function showIllusionCovariateQuality(drawingSnips, disparity, canRate, coutSnips, coutDisp, canCout)
+function showIllusionCovariateQuality(drawingSnips, disparity, canRate)%, coutSnips, coutDisp, canCout)
 
 
 %%%%%%%%%%%%%%%%%
@@ -13,17 +13,19 @@ for day=1:length(canRate)
         right = canRate{day}.canonical03(trial,:);
         left(1:20) = 0;
         left(end-19:end) = 0;
-        xc = xc + xcov(left, right, 20, 'coef');
+        xc = xc + xcov(left, right, 10, 'coef');
     end
     xc = xc / sum(sel);
     quality{day} = xc;
 end
 quality = cell2mat(quality);
 
-subplot(3,1,1);
-plot(-20:20,quality','k');
-set(gca,'XTick',[-20 0 20]);
-set(gca,'XTickLabel',[-1 0 1]);
+subplot(2,1,1);
+plot(-10:10,quality','k');
+set(gca,'XTick',[-10 0 10]);
+set(gca,'XTickLabel',[-.5 0 .5]);
+ylim([min(quality(:))-.1 max(quality(:))+.1]);
+% xlabel('Time (cycles)');
 ylabel('Cross-correlation');
 title('Illusion');
 
@@ -42,7 +44,7 @@ for day=1:length(canRate)
         right = canRate{day}.canonical03(trial,:);
         left(1:20) = 0;
         left(end-19:end) = 0;
-        xc = xc + xcov(left, right, 20, 'coef');
+        xc = xc + xcov(left, right, 10, 'coef');
     end
     xc = xc / sum(sel);
     time{day} = mean(drawingSnips{day}.time(sel,:));
@@ -50,10 +52,11 @@ for day=1:length(canRate)
 end
 quality = cell2mat(quality);
 
-subplot(3,1,2);
-plot(-20:20,quality','k');
-set(gca,'XTick',[-20 0 20]);
-set(gca,'XTickLabel',[-1 0 1]);
+subplot(2,1,2);
+plot(-10:10,quality','k');
+set(gca,'XTick',[-10 0 10]);
+set(gca,'XTickLabel',[-.5 0 .5]);
+ylim([min(quality(:))-.1 max(quality(:))+.1]);
 xlabel('Time (cycles)');
 ylabel('Cross-correlation');
 title('Non-illusion');
@@ -61,27 +64,27 @@ title('Non-illusion');
 %%%%%%%%%%%%%%%%%
 % Cout trials
 %%%%%%%%%%%%%%%%%
-quality = cell(numel(canRate),1);
-time = cell(size(quality));
-for day=1:length(canRate)
-    xc = 0;
-    for trial=1:size(coutSnips{day}.time,1)
-        left = coutDisp{day}.x(trial,:);
-        right = canCout{day}.canonical03(trial,:);
-        xc = xc + xcov(left, right, 10, 'coef');
-    end
-    xc = xc / sum(sel);
-    time{day} = mean(coutSnips{day}.time(sel,:));
-    quality{day} = xc;
-end
-quality = cell2mat(quality);
-
-subplot(3,1,3);
-plot(-10:10,quality','k');
-set(gca,'XTick',[-10 0 10]);
-set(gca,'XTickLabel',[-1 0 1]);
-xlabel('Time (reach)');
-ylabel('Cross-correlation');
-title('C.-out / out-c.');
-
-set(gcf,'PaperPosition',[1 1 3.35 3.35*1.5]);
+% quality = cell(numel(canRate),1);
+% time = cell(size(quality));
+% for day=1:length(canRate)
+%     xc = 0;
+%     for trial=1:size(coutSnips{day}.time,1)
+%         left = coutDisp{day}.x(trial,:);
+%         right = canCout{day}.canonical03(trial,:);
+%         xc = xc + xcov(left, right, 10, 'coef');
+%     end
+%     xc = xc / size(coutSnips{day}.time,1);
+%     time{day} = mean(coutSnips{day}.time(sel,:));
+%     quality{day} = xc;
+% end
+% quality = cell2mat(quality);
+% 
+% subplot(3,1,3);
+% plot(-10:10,quality','k');
+% set(gca,'XTick',[-10 0 10]);
+% set(gca,'XTickLabel',[-1 0 1]);
+% xlabel('Time (reach)');
+% ylabel('Cross-correlation');
+% title('C.-out / out-c.');
+% 
+set(gcf,'PaperPosition',[1 1 3.35 3.35]);
